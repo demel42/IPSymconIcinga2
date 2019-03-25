6853a00f3b2965ee01dd17d84151d84204ef4bc2
demel42 <demel42@web.de>
2019-03-25 08:46:16 +0100

    *.display_name verwenden

--- docs/event-script.php
+++ docs/event-script.php
@@ -13,12 +13,12 @@ $attempt = $_IPS['attempt'];
 $mode = $_IPS['mode'];
 switch ($mode) {
     case 'host':
-        $host = $_IPS['host'];
+        $host = $_IPS['host_name'];
         IPS_LogMessage($scriptName, 'host ' . $host . ': state=' . $state . ', type=' . $type);
         break;
     case 'service':
-        $host = $_IPS['host'];
-        $service = $_IPS['service'];
+        $host = $_IPS['host_name'];
+        $service = $_IPS['service_name'];
         IPS_LogMessage($scriptName, 'service ' . $host . '!' . $service . ': state=' . $state . ', type=' . $type);
         break;
     default:
